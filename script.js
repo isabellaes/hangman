13f6b210044e7d756d3b02207137e9942eae8278
arvodeman@gmail.com <arvodeman@gmail.com>
2023-10-10 14:58:51 +0200

    lade in svg, skapde css klasser

---
+++ script.js
@@ -0,0 +1,8 @@
+// 6st gisningar
+//för varje gisning 
+// länka till en ny del av SVG filen
+//gisningNR 1 = svg id¨
+// display Item
+//Dela upp SVG
+// get elementby .id 
+//display block, display none
