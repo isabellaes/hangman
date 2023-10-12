5c8fead9db741c83b7933b83841ecc2cbb2eb2bd
isabellaes <borjessonisabella@gmail.com>
2023-10-12 11:42:21 +0200

    win and loose page

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const attemptsDisplay = document.querySelector(".attempts-display");
 const buttons = document.querySelectorAll(".buttons button");
 
 //array för svg
-const hmanSvg=['ground','head','scaffold','legs','arms','body',];
+const hmanSvg = ["ground", "head", "scaffold", "legs", "arms", "body"];
 hmanSvg.reverse();
 // Uppdatera visningen av ordet med gissade bokstäver
 function updateWordDisplay() {
@@ -53,16 +53,14 @@ function updateGuessedWord(letter) {
 // Kolla om spelaren har vunnit
 function checkWin() {
   if (!guessedWord.includes("_")) {
-    alert("Grattis, du har vunnit!");
-    resetGame();
+    window.location.href = "win.html";
   }
 }
 
 // Kolla om spelaren har förlorat
 function checkLose() {
   if (remainingAttempts === 0) {
-    Document.querySelector("Du har förlorat. Rätt ord var: " + selectedWord);
-    resetGame();
+    window.location.href = "loose.html";
   }
 }
 
@@ -73,13 +71,13 @@ function handleGuess(letter) {
 
   if (isLetterInWord(letter)) {
     updateGuessedWord(letter);
-    checkWin();
+
     buttonElement.style.backgroundColor = "green"; // Ändra knappens färg till grön
   } else {
     remainingAttempts--;
     hManDsiplay();
     updateAttemptsDisplay();
-    
+
     buttonElement.style.backgroundColor = "red"; // Ändra knappens färg till röd
   }
 }
@@ -105,30 +103,29 @@ function startGame() {
   updateAttemptsDisplay();
 }
 
-function hManDsiplay (){
-
-  console.log(hmanSvg)
-  const id = hmanSvg[remainingAttempts ];
-  console.log(id)
- // Skriva ut svg filen
+function hManDsiplay() {
+  console.log(hmanSvg);
+  const id = hmanSvg[remainingAttempts];
+  console.log(id);
+  // Skriva ut svg filen
   const element = document.getElementById(id);
   element.style.display = "block";
   console.log(element);
   console.log(remainingAttempts);
- // få rätt id på arrayn
-  
- // ta bort ur arrayn
-  
- // när arrayn är tom- skicka till you lose sidan
+  // få rätt id på arrayn
 
+  // ta bort ur arrayn
+
+  // när arrayn är tom- skicka till you lose sidan
 }
-
 
 // Lyssna på klickhändelser på knapparna
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", () => {
     const letter = buttons[i].innerText;
     handleGuess(letter);
+    checkLose();
+    checkWin();
   });
 }
 
